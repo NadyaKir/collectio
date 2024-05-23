79f54181f810da16ac0402246a4b8d1f041d7034
Nadezhda Kireyeva <styyypik@gmail.com>
2024-05-23 22:13:35 +0500

    styles: mb-4 for Home page h2

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,15 +6,15 @@ export default function HomePage() {
   return (
     <>
       <div className="mb-8">
-        <h2 className="text-xl font-bold mb-2">Last items</h2>
+        <h2 className="text-xl font-bold mb-4">Last items</h2>
         <LastItems />
       </div>
       <div className="mb-8">
-        <h2 className="text-xl font-bold mb-2">Top biggest collections</h2>
+        <h2 className="text-xl font-bold mb-4">Top biggest collections</h2>
         <TopCollections />
       </div>
       <div className="items-center">
-        <h2 className="text-xl font-bold mb-2">Tags cloud</h2>
+        <h2 className="text-xl font-bold mb-4">Tags cloud</h2>
         <TagsCloud />
       </div>
     </>
