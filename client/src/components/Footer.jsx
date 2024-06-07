414455686808591e02248af177d1ba24bce2b5ab
Nadezhda Kireyeva <styyypik@gmail.com>
2024-06-07 18:32:37 +0500

    feat: show create ticket for authenticated users only

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import TicketModal from "./TicketModal";
 
 export default function Footer() {
   const [isOpen, setOpen] = useState(false);
+
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   const openModal = () => {
     setOpen(true);
@@ -13,14 +16,18 @@ export default function Footer() {
   };
 
   return (
-    <div className="flex justify-center py-4">
-      {isOpen && <TicketModal closeModal={closeModal} />}
-      <button
-        className="text-sm text-gray-600 no-underline hover:underline"
-        onClick={openModal}
-      >
-        Create support ticket
-      </button>
-    </div>
+    <>
+      {isAuthenticated && (
+        <div className="flex justify-center py-4">
+          {isOpen && <TicketModal closeModal={closeModal} />}
+          <button
+            className="text-sm text-gray-600 no-underline hover:underline"
+            onClick={openModal}
+          >
+            Create support ticket
+          </button>
+        </div>
+      )}
+    </>
   );
 }
